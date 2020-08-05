057c22b7f79511e4d4fe60343ce42102bdc1d817
Digvijay Rathore <rathore.digvijay10@gmail.com>
2020-08-05 18:03:36 +0530

    pagination, sorting and other changes in API routes

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,10 +1,14 @@
 const express = require('express')
-const Task = require('../models/task')
+const Task = require('../models/task');
+const auth = require('../middleware/auth');
 const router = new express.Router()
 
 // API Route: Create New Task
-router.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
 
     try {
         await task.save()
@@ -14,22 +18,41 @@ router.post('/tasks', async (req, res) => {
     }
 })
 
-// API Route: Get Tasks
-router.get('/tasks', async (req, res) => {
+// API Route: Get All Tasks 
+// Exapmle Route - GET /tasks?completd:true
+// Exapmle Route - GET /tasks?limit=10&skip=10
+// Exapmle Route - GET /tasks?sortBy=createdAt:desc
+router.get('/tasks', auth, async (req, res) => {
+    const match = {};
+    const sort = {};
+    if(req.query.completed){
+        match.completed = req.query.completed == 'true';
+    }
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit : parseInt(req.query.limit),
+                skip: parseInt(req.query.skip)
+            }
+        }).execPopulate();
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
 })
 
 // API Route: Get specific task
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id });
 
         if (!task) {
             return res.status(404).send()
@@ -42,7 +65,7 @@ router.get('/tasks/:id', async (req, res) => {
 })
 
 // API Route: Update Specific task
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -52,14 +75,14 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
-
-        updates.forEach((update) => task[update] = req.body[update])
-        await task.save()
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send()
         }
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
 
         res.send(task)
     } catch (e) {
@@ -68,9 +91,9 @@ router.patch('/tasks/:id', async (req, res) => {
 })
 
 // API Route: Delete Specific Task
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
 
         if (!task) {
             res.status(404).send()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth.js');
-const router = new express.Router()
+const router = new express.Router();
 
 // API Route: Create user
 router.post('/users', async (req, res) => {
@@ -28,6 +28,7 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
+// API Route: Logout from single session
 router.post('/users/logout', auth, async (req, res) => {
     try {
         req.user.token = req.user.tokens.filter((token) => {
@@ -42,6 +43,7 @@ router.post('/users/logout', auth, async (req, res) => {
     }
 });
 
+// API Route: Logout from all the sessions
 router.post('/users/logoutAll', auth, async (req, res) => {
     try {
         req.user.token = [];
@@ -57,25 +59,9 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
 })
 
-// API Route: Find specific user by Id
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 
 // API Route: Update Specific user by Id
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -85,31 +71,19 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findById(req.params.id)
-
-        updates.forEach((update) => user[update] = req.body[update])
-        await user.save()
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update]);
+        await req.user.save();
+        res.send(req.user)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
 // API Route: Delete Specific user by Id
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        await req.user.remove();
+        res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
