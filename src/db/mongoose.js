756ce39d22f46313f4f4989ef91e9624cf7590b0
Digvijay <rathore.digvijay10@gmail.com>
2020-09-13 17:07:18 +0530

    small fix in connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('process.env.MONGO_URL', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
